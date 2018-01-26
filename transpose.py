e708f320e90be9d6d7c5689dc5828e320313f19f
shanyaanand <anandshanya97@gmail.com>
2018-01-26 13:55:37 +0000

    Delete transpose.py

--- transpose.py
+++
@@ -1,7 +0,0 @@
-def transpose(inp, out):
-	with open(inp) as f:
-		lis = [x.split(",") for x in f]
-	
-	for x in zip(*lis):
-		with open(out, "a+") as op:
-			op.write( ",".join( [y for y in x] ) + "\n" )
